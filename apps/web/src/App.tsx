3552ece991bcc9b8a28f0880e8638851bd62be74
Shaul Lavochkin <shaul_lavochkin@intuit.com>
2025-12-07 16:17:46 +0200

    add dark mode manager

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,21 +1,33 @@
 import { type Component, onMount } from 'solid-js'
+import {
+	ColorModeProvider,
+	ColorModeScript,
+	createLocalStorageManager
+} from '@kobalte/core'
 import Main from './Main'
 import { Toaster } from '@repo/ui/toaster'
 import { FsProvider } from './fs/context/FsProvider'
 import { FocusProvider } from './focus/focusManager'
 import { pingServerRoutes } from '~/serverRoutesProbe'
+
+const storageManager = createLocalStorageManager('vite-ui-theme')
 const App: Component = () => {
 	onMount(() => {
 		void pingServerRoutes()
 	})
 
 	return (
-		<FocusProvider>
-			<FsProvider>
-				<Main />
-				<Toaster />
-			</FsProvider>
-		</FocusProvider>
+		<>
+			<ColorModeScript storageType={storageManager.type} />
+			<ColorModeProvider storageManager={storageManager}>
+				<FocusProvider>
+					<FsProvider>
+						<Main />
+						<Toaster />
+					</FsProvider>
+				</FocusProvider>
+			</ColorModeProvider>
+		</>
 	)
 }
 
