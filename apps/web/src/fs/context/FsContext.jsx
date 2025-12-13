28eafb2565766269b1e5eade2f9b8ee2682ea985
Shauls <shaul919191@gmail.com>
2025-12-13 21:27:34 +0200

    refactor: Delete numerous UI components and contexts, and refactor the main application routing.

--- apps/web/src/fs/context/FsContext.jsx
+++
@@ -1,9 +0,0 @@
-import { createContext, useContext } from "solid-js";
-export const FsContext = createContext();
-export function useFs() {
-    const ctx = useContext(FsContext);
-    if (!ctx) {
-        throw new Error("useFs must be used within an FsProvider");
-    }
-    return ctx;
-}
