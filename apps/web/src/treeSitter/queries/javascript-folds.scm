8aaec25a87fe55a048bf969851e974428de7383a
Shauls <shaul919191@gmail.com>
2025-12-17 21:45:38 +0200

    feat: Add folding support for control flow statements to JavaScript and TypeScript Tree-sitter queries.

--- apps/web/src/treeSitter/queries/javascript-folds.scm
+++ apps/web/src/treeSitter/queries/javascript-folds.scm
@@ -1,3 +1,4 @@
+; Functions and Classes
 [
   (function_declaration)
   (class_declaration)
@@ -5,4 +6,20 @@
   (arrow_function)
   (object)
   (import_statement)
+] @fold
+
+; Control flow statements
+[
+  (if_statement)
+  (else_clause)
+  (for_statement)
+  (for_in_statement)
+  (while_statement)
+  (do_statement)
+  (switch_statement)
+  (switch_case)
+  (switch_default)
+  (try_statement)
+  (catch_clause)
+  (finally_clause)
 ] @fold
--- apps/web/src/treeSitter/queries/typescript-folds.scm
+++ apps/web/src/treeSitter/queries/typescript-folds.scm
@@ -1,10 +1,11 @@
+; TypeScript-specific declarations
 (interface_declaration) @fold
 (type_alias_declaration) @fold
 (enum_declaration) @fold
 (internal_module) @fold
 (module) @fold
 
-; Inherit JS folds
+; Functions and Classes (inherited from JS)
 [
   (function_declaration)
   (class_declaration)
@@ -12,4 +13,20 @@
   (arrow_function)
   (object)
   (import_statement)
+] @fold
+
+; Control flow statements
+[
+  (if_statement)
+  (else_clause)
+  (for_statement)
+  (for_in_statement)
+  (while_statement)
+  (do_statement)
+  (switch_statement)
+  (switch_case)
+  (switch_default)
+  (try_statement)
+  (catch_clause)
+  (finally_clause)
 ] @fold
